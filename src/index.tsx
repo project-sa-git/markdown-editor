479f90a33d19703b13c8fd18704b278d0a7f06a4
mymac <trybook1901@gmail.com>
2022-08-12 21:59:58 +0900

    2-2 エディタ画面の見た目を作る

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
 
-const Header = styled.h1`
-    color: red;
+// ページ全体に適用できるスタイルを定義
+const GlobalStyle = createGlobalStyle`
+    body * {
+      box-sizing: border-box;
+    }
   `
- 
-  const Main = (<Header>Markdown Editor</Header>)
+  
+  const Main = (
+    <>
+      <GlobalStyle />
+      <Editor />
+    </>
+  )
 
 render(Main, document.getElementById('app'))
